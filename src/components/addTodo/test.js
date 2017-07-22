07b77adf303b66a9e3630cfeab22e39d816a03cd
Albert Yanit <ayanit1@googlemail.com>
2017-07-22 21:55:59 +0100

    add input to component

--- src/components/addTodo/test.js
+++ src/components/addTodo/test.js
@@ -1,12 +1,17 @@
 /* global expect, it, describe */
 
 import React from 'react';
-import { shallow } from 'enzymne';
+import { shallow } from 'enzyme';
 import AddTodo from '.';
 
 describe('AddTodo component', () => {
   it('should render sucessfully', () => {
     const component = shallow(<AddTodo />);
     expect(component.exists()).toEqual(true);
+  });
+
+  it('should have one input', () => {
+    const component = shallow(<AddTodo />);
+    expect(component.find('todo-input').length).toEqual(1);
   });
 });
